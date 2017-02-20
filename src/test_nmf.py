58c3f3e0282cd30db46ba223f0524070174c489a
mostafa-ashraf <mostafa.ashraf@adaptcentre.ie>
2017-02-21 00:32:45 +0300

    test_nmf

--- src/test_nmf.py
+++ src/test_nmf.py
@@ -3,13 +3,23 @@ import random
 from deap import tools
 import numpy as np
 
+V = [[0,1,0,1,0],
+     [1,0,1,0,1],
+     [0,1,0,0,0],
+     [1,0,0,0,1],
+     [0,1,0,1,0]]
+
+#V = [[0,1,0],
+#     [1,0,1],
+#     [0,1,0]]
+
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("Individual", np.ndarray, fitness=creator.FitnessMin)
 
-IND_SIZE = 1
+IND_SIZE = 1#len(V), len(V[0])
 
 def genIndividual():
-    return np.random.rand(5,5)
+    return np.random.rand(len(V), len(V[0]))
 
 toolbox = base.Toolbox()
 toolbox.register("attribute", genIndividual)
@@ -19,17 +29,36 @@ toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 def evaluate(individual):
     V_ = individual.dot(individual.T)
     dis = V-V_
-    return np.linalg.norm(dis),
+    fit = np.linalg.norm(dis)
+    return fit,
 
-toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.1)
+def mCX(ind1_, ind2_):
+    ind1, ind2 = ind1_[0], ind2_[0]
+    cX_point = len(ind1)/2
+    ind1[:cX_point], ind2[:cX_point] = ind2[:cX_point].copy(), ind1[:cX_point].copy()
+    return ind1_, ind2_
+    
+def mMut(ind, mu, sigma, indpb):
+    rInd = ind[0].reshape(len(ind[0])*len(ind[0][0]))
+    tools.mutGaussian(rInd, mu, sigma, indpb)
+    return ind
+
+toolbox.register("mate", mCX) #tools.cxTwoPoint)
+toolbox.register("mutate", mMut, mu=0, sigma=1, indpb=0.2)#tools.mutGaussian, mu=0, sigma=1, indpb=0.1)
 toolbox.register("select", tools.selTournament, tournsize=3)
 toolbox.register("evaluate", evaluate)
 
+# statistics registeration
+stats = tools.Statistics(key=lambda ind: ind.fitness.values)
+stats.register("avg", np.mean)
+stats.register("std", np.std)
+stats.register("min", np.min)
+stats.register("max", np.max)
+#
 
 def main():
-    pop = toolbox.population(n=50)
-    CXPB, MUTPB, NGEN = 0.5, 0.2, 40
+    pop = toolbox.population(n=100)
+    CXPB, MUTPB, NGEN = 0.9, 0.2, 5000
 
     # Evaluate the entire population
     fitnesses = map(toolbox.evaluate, pop)
@@ -41,18 +70,18 @@ def main():
         offspring = toolbox.select(pop, len(pop))
         # Clone the selected individuals
         offspring = map(toolbox.clone, offspring)
+        
+        # Apply crossover and mutation on the offspring
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+            if random.random() < CXPB:
+                toolbox.mate(child1, child2)
+                del child1.fitness.values
+                del child2.fitness.values
 
-#        # Apply crossover and mutation on the offspring
-#        for child1, child2 in zip(offspring[::2], offspring[1::2]):
-#            if random.random() < CXPB:
-#                toolbox.mate(child1, child2)
-#                del child1.fitness.values
-#                del child2.fitness.values
-#
-#        for mutant in offspring:
-#            if random.random() < MUTPB:
-#                toolbox.mutate(mutant)
-#                del mutant.fitness.values
+        for mutant in offspring:
+            if random.random() < MUTPB:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
 
         # Evaluate the individuals with an invalid fitness
         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
@@ -62,15 +91,13 @@ def main():
 
         # The population is entirely replaced by the offspring
         pop[:] = offspring
-
+        # printing statistics
+        record = stats.compile(pop)
+        print "gen #%d: stats:%s"%(g, str(record['min']))
     return pop
-
-V = [[0,1,0,1,0],
-     [1,0,1,0,1],
-     [0,1,0,0,0],
-     [1,0,0,0,1],
-     [0,1,0,1,0]]
-      
+            
 pop = main()
 
-print pop
+#print pop
+
+minInd = min(pop , key = lambda ind: ind.fitness.values[0])
